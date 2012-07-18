ef7a1d02980899c545e8fe98ae856447be74ce14
Giovanni Sakti <giosakti@starqle.com>
2012-07-18 14:25:44 +0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '262a18acd2a653e99cca125d3eb24059b74fdb7edda23917ccf816122b9f1d957291742c6aaed3c08bf9033b24c7964171f416d89890bd700f0d8ef08f2a8ea9'
